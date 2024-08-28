3a83ceae3a54825b0690691c6e41ceeb1cc27a25
Cole Cantu <colecantu904@gmail.com>
2024-08-28 15:28:41 -0700

    intial files added

---
+++ svelt/src/App.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+
+
+  // choosing color logic will be here
+  let color1: string = "red";
+  let color2: string = "blue";
+
+
+</script>
+
+<main>
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+  <div class="button-container">
+    <button class="color-button" --color={color1}>Red</button>
+    <button class="color-button" --color={color2}>Blue</button>
+  </div>
+
+</main>
+
+<style>
+  .read-the-docs {
+    color: #888;
+  }
+
+  .button-container {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+  }
+
+  .color-button {
+    width: 50vw;
+    height: 90vh;
+    background-color: var(--color, #1a1a1a);
+}
+
+</style>
---
+++ svelt/src/colorBox.svelte
@@ -0,0 +1,10 @@
+<button class="color-button" >Red</button>
+
+<style>
+    .color-button {
+      width: 50vw;
+      height: 90vh;
+      background-color: var(--color, #1a1a1a);
+  }
+</style>
+  
